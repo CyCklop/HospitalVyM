0cb7526c152ff744ef48251a6de17abb0669b034
CyCklop <ignacioandresflores06@gmail.com>
2026-04-29 23:01:58 -0400

    update/paciente-service

--- src/main/java/com/hospital_vm_cl/hospital_vm/service/PacienteService.java
+++ src/main/java/com/hospital_vm_cl/hospital_vm/service/PacienteService.java
@@ -1,5 +1,32 @@
 package com.hospital_vm_cl.hospital_vm.service;
+import com.hospital_vm_cl.hospital_vm.model.Paciente;
+import com.hospital_vm_cl.hospital_vm.repository.PacienteRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class PacienteService {
+    @Autowired
+    private PacienteRepository pacienteRepo;
 
+    public List<Paciente> findAll(){
+        return pacienteRepo.findAll();
+    }
+
+    public Paciente findById(Integer id){
+        return pacienteRepo.findById(id).get();
+    }
+
+    public Paciente save(Paciente paciente){
+        return pacienteRepo.save(paciente);
+    }
+
+    public void delete(Integer id){
+        pacienteRepo.deleteById(id);
+    }
 }
